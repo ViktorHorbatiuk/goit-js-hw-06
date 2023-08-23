673e693bad796b252d2759cbe9a3b32b6a37f075
ViktorHorbatiuk <horbatiuk.va@gmail.com>
2023-08-23 23:31:32 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,14 @@
+const buttonOfColorChenge = document.querySelector(".change-color");
+const colorOfText = document.querySelector(".color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+buttonOfColorChenge.addEventListener("click", function () {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  colorOfText.textContent = randomColor;
+});
