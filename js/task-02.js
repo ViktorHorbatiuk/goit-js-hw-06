1f161ac5a84a93bfd825914c5b52aede4d602ecf
ViktorHorbatiuk <horbatiuk.va@gmail.com>
2023-08-23 15:49:50 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listOfIngredients = document.querySelector("#ingredients");
+
+const itemsOfList = ingredients.map((ingredient) => {
+  const item = document.createElement("li");
+  item.textContent = ingredient;
+  item.classList.add("item");
+  return item;
+});
+
+listOfIngredients.append(...itemsOfList);
