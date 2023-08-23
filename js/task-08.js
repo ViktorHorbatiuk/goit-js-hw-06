e7eb88a6fee098c145b38a6171115d149cf54a09
ViktorHorbatiuk <horbatiuk.va@gmail.com>
2023-08-23 23:09:11 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const formOfLogin = document.querySelector(".login-form");
 
+formOfLogin.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const inputOfEmail = formOfLogin.elements.email;
+  const inputOfPassword = formOfLogin.elements.password;
+
+  if (inputOfEmail.value === "" || inputOfPassword.value === "") {
+    alert("Please fill in all the fields!");
+  } else {
+    const formData = {
+      email: inputOfEmail.value,
+      password: inputOfPassword.value,
+    };
+
+    console.log(formData);
+    formOfLogin.reset();
+  }
+});
